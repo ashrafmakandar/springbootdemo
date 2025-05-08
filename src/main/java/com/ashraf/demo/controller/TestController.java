413a8bdd004c8ce468d2255c138cac9c5ad87e42
ashrafmakandar <37826738+ashrafmakandar@users.noreply.github.com>
2025-05-08 22:14:42 +0530

    chnages in pom file

---
+++ src/main/java/com/ashraf/demo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.ashraf.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Railway + Spring Boot!";
+    }
+}
